2c87acf93e203ecc999eb734d588b998ea8d33e1
Tomasz.Porst <Tomasz.Porst@adtechfactory.com>
2022-02-09 15:21:47 +0100

    padding for scrollspy item

--- src/components/ItemScrollSpy.js
+++ src/components/ItemScrollSpy.js
@@ -7,25 +7,25 @@ const ItemScrollSpy = (props) =>{
     const dispatch = useDispatch();
 
     useEffect(() => {
-        
+
         console.log('ItemScrollSpy is rendered '+props.scrollSpyId);
-        let padding = 250;
-        let navBarPadding = 65;
+        let paddingTop = props.paddingTop ? props.paddingTop : 0;
+        let paddingBottom = props.paddingBottom ? props.paddingBottom : 0;
         let hight = document.querySelector('#'+props.scrollSpyId).offsetHeight;
-        let top =  document.querySelector('#'+props.scrollSpyId).offsetTop - navBarPadding;
-        console.log({[props.scrollSpyId]: [top - padding, top + hight - padding]} );
+        let top =  document.querySelector('#'+props.scrollSpyId).offsetTop;
+        console.log({[props.scrollSpyId]: [top, top + hight ]} );
         //set the position in global store only one time, which is avoiding infinity re-rendering of components
         if(props.scrollSpy){
-            props.setScrollSpy(false); 
+            props.setScrollSpy(false);
             console.log('ItemScrollSpy dispatched ');
             //set component scroll position in global store, so that the manu item can be highlighted when user scroll to this position
-            dispatch(setComptPositions({[props.scrollSpyId]: [top - padding, top + hight - padding]}));
+            dispatch(setComptPositions({[props.scrollSpyId]: [top + paddingTop, top + hight + paddingBottom]}));
         }
-    
+
     });
-     
+
     return(
-        <div id={props.scrollSpyId}>         
+        <div id={props.scrollSpyId}>
         {props.children}
         </div>)
 }
